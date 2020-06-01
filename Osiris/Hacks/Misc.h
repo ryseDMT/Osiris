c2638d7fa0a2f1ea6baf867dbd5290fed11b7209
Mihail Dimitrov <ryse_va@mail.bg>
2020-06-01 14:36:51 +0300

    zzz

---
+++ Osiris/Hacks/Misc.h
@@ -0,0 +1,51 @@
+#pragma once
+
+enum class FrameStage;
+class GameEvent;
+struct UserCmd;
+
+namespace Misc
+{
+    void edgejump(UserCmd* cmd) noexcept;
+    void slowwalk(UserCmd* cmd) noexcept;
+    void inverseRagdollGravity() noexcept;
+    void updateClanTag(bool = false) noexcept;
+    void spectatorList() noexcept;
+    void sniperCrosshair() noexcept;
+    void recoilCrosshair() noexcept;
+    void watermark() noexcept;
+    void prepareRevolver(UserCmd*) noexcept;
+    void fastPlant(UserCmd*) noexcept;
+    void drawBombTimer() noexcept;
+    void stealNames() noexcept;
+    void disablePanoramablur() noexcept;
+    void quickReload(UserCmd*) noexcept;
+    bool changeName(bool, const char*, float) noexcept;
+    void bunnyHop(UserCmd*) noexcept;
+    void fakeBan(bool = false) noexcept;
+    void resetName(bool set) noexcept;
+    void fakeItem(bool set) noexcept;
+    void resetName(bool set) noexcept;
+    void fakeItem(bool set) noexcept;
+    void nadePredict() noexcept;
+    void quickHealthshot(UserCmd*) noexcept;
+    void fixTabletSignal() noexcept;
+    void fakePrime() noexcept;
+    void killMessage(GameEvent& event) noexcept;
+    void fixMovement(UserCmd* cmd, float yaw) noexcept;
+    void antiAfkKick(UserCmd* cmd) noexcept;
+    void fixAnimationLOD(FrameStage stage) noexcept;
+    void autoPistol(UserCmd* cmd) noexcept;
+    float RandomFloat(float min, float max) noexcept;
+    void chokePackets(bool& sendPacket, UserCmd* cmd) noexcept;
+    void autoReload(UserCmd* cmd) noexcept;
+    void revealRanks(UserCmd* cmd) noexcept;
+    void autoStrafe(UserCmd* cmd) noexcept;
+    void removeCrouchCooldown(UserCmd* cmd) noexcept;
+    void moonwalk(UserCmd* cmd) noexcept;
+    void playHitSound(GameEvent& event) noexcept;
+    void drawAimbotFov() noexcept;
+    void killSound(GameEvent& event) noexcept;
+    void purchaseList(GameEvent* event = nullptr) noexcept;
+    void drawBombDamage() noexcept;
+}
---
+++ Osiris/Hacks/Visuals.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "../SDK/Vector.h"
+enum class FrameStage;
+class GameEvent;
+
+namespace Visuals
+{
+    void playerModel(FrameStage stage) noexcept;
+    void colorWorld() noexcept;
+    void Fullbright() noexcept;
+    void modifySmoke() noexcept;
+    void thirdperson(FrameStage stage, Vector angle) noexcept;
+    void removeVisualRecoil(FrameStage stage) noexcept;
+    void removeBlur() noexcept;
+    void updateBrightness() noexcept;
+    void removeGrass() noexcept;
+    void remove3dSky() noexcept;
+    void removeShadows() noexcept;
+    void applyZoom(FrameStage stage) noexcept;
+    void applyScreenEffects() noexcept;
+    void hitEffect(GameEvent* event = nullptr) noexcept;
+    void hitMarker(GameEvent* event = nullptr) noexcept;
+    void disablePostProcessing() noexcept;
+    void reduceFlashEffect() noexcept;
+    bool removeHands(const char* modelName) noexcept;
+    bool removeSleeves(const char* modelName) noexcept;
+    bool removeWeapons(const char* modelName) noexcept;
+    void skybox() noexcept;
+}
